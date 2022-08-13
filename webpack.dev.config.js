85c6997414beeb32312dfb94078319193e79fc69
supercll <1033581609@qq.com>
2022-08-13 16:41:26 +0800

    feat: dev

---
+++ webpack.dev.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    'iacg-cli-monitor': path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, './examples/index.html'),
+      chunks: ['iacg-cli-monitor'],
+      scriptLoading: 'blocking',
+      inject: 'head',
+    }),
+  ],
+}
