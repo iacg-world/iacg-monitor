8dc4affbcbe394217d89db03751451fb60209c4b
supercll <1033581609@qq.com>
2023-07-13 19:02:24 +0800

    refactor: performance time

--- src/perf.js
+++ src/perf.js
@@ -1,58 +1,176 @@
 import {
   sendPerf,
 } from './collect'
-let fp = 0 // first-paint
-let fcp = 0 // first-contentful-paint
-let lcp = 0 // largest-contentful-paint
-let dns = 0
-let tcp = 0
-let http = 0
-let dom = 0
-let load = 0
-function callback(perf) {
-  perf.getEntries().forEach(entry => {
-    console.log('entry: ', entry)
-    if (entry.name === 'first-paint') {
-      fp = entry.startTime
-    } else if (entry.name === 'first-contentful-paint') {
-      fcp = entry.startTime
-    } else if (entry.entryType === 'largest-contentful-paint') {
-      lcp = entry.startTime
+
+/**
+ * PerformanceObserver 监听性能类型。兼容性：IE不支持，safari 12.1及以上，安卓 5及以上
+ * @param type
+ * @param callback
+ */
+export const observe = (
+  type,
+  callback,
+) => {
+  try {
+    const PerformanceObserver = window.PerformanceObserver;
+    if (
+      PerformanceObserver &&
+      PerformanceObserver.supportedEntryTypes &&
+      PerformanceObserver.supportedEntryTypes.indexOf(type) !== -1
+    ) {
+      const po = new PerformanceObserver((l) => callback(l.getEntries()));
+      po.observe({ type, buffered: true }); // type：观察的类型；buffered: 是否应将缓冲的条目排队到观察者的缓冲区中。
+      return po;
     }
-    
-    if (entry.entryType === 'navigation') {
-      dns = entry.domainLookupEnd - entry.domainLookupStart;
-      tcp = entry.connectEnd - entry.connectStart;
-      http = entry.responseEnd - entry.requestStart;
-      dom = entry.domComplete - entry.domInteractive;
-      load = entry.loadEventEnd - entry.loadEventStart;
-      // console.log('dns', dns);
-      // console.log('tcp', tcp);
-      // console.log('http', http);
-      // console.log('dom', dom);
-      // console.log('load', load);
+  } catch {
+    // 根据w3c结论，observe的参数entryTypes和type不可同时出现，但部分浏览器无entryTypes参数会报错。所以忽略这里报错
+    // https://github.com/w3c/performance-timeline/pull/112
+  }
+  return false;
+};
+
+/**
+* FCP时间
+* @returns {Promise<number>}
+*/
+const getFCP = () => {
+  return new Promise((resolve) => {
+    const entryHandler = (entryList) => {
+      const timeList = [];
+      entryList.forEach(({ name, startTime }) => {
+        if (name === 'first-contentful-paint') {
+          timeList.push(Math.floor(startTime));
+        }
+      });
+      resolve(timeList.pop() || 0);
+    };
+    const po = observe('paint', entryHandler);
+    if (!po) {
+      resolve(0);
     }
-  })
-  // console.log('fp', fp)
-  // console.log('fcp', fcp)
-  // console.log('lcp', lcp)
+
+  });
+};
+
+/**
+* LCP时间
+* @returns {Promise<number>}
+*/
+const getLCP = () => {
+  return new Promise((resolve) => {
+    const entryHandler = (entryList) => {
+      const timeList = [];
+      entryList.forEach(({ startTime }) => {
+        timeList.push(Math.floor(startTime));
+      });
+      resolve(timeList.pop() || 0);
+    };
+    const po = observe('largest-contentful-paint', entryHandler);
+    if (!po) {
+      resolve(0);
+    }
+
+  });
+};
+
+/**
+* FID时间
+* @returns {Promise<number>}
+*/
+const getFID = () => {
+  return new Promise((resolve) => {
+    try {
+      const entryHandler = (entryList) => {
+        const timeList = [];
+        entryList.forEach(({ duration }) => {
+          timeList.push(Math.floor(duration));
+        });
+        resolve(timeList.pop() || 0);
+      };
+      const po = observe('first-input', entryHandler);
+      if (!po) {
+        resolve(0);
+      }
+    } catch (err) {
+    }
+  });
+};
+/**
+* FP时间
+* @returns {Promise<number>}
+*/
+const getFP = () => {
+  return new Promise((resolve) => {
+    try {
+      const entryHandler = (entryList) => {
+        const timeList = [];
+        entryList.forEach(({ duration }) => {
+          timeList.push(Math.floor(duration));
+        });
+        resolve(timeList.pop() || 0);
+      };
+      const po = observe('first-paint', entryHandler);
+      if (!po) {
+        resolve(0);
+      }
+    } catch (err) {
+    }
+  });
+};
+/**
+* navigation时间
+* @returns {Promise<number>}
+*/
+const getNavigation = () => {
+  return new Promise((resolve) => {
+    try {
+      const d = {
+        dns: 0,
+        tcp: 0,
+        http: 0,
+        dom: 0,
+        load: 0,
+      }
+      const entryHandler = (entryList) => {
+        const navigationObjList = []
+        entryList.forEach((entry) => {
+          const navigationObj = {
+            dns: entry.domainLookupEnd - entry.domainLookupStart,
+            tcp: entry.connectEnd - entry.connectStart,
+            http: entry.responseEnd - entry.requestStart,
+            dom: entry.domComplete - entry.domInteractive,
+            load: entry.loadEventEnd - entry.loadEventStart,
+          }
+          navigationObjList.push(navigationObj)
+        });
+
+        resolve(navigationObjList.pop() || d);
+      };
+      const po = observe('navigation', entryHandler);
+      if (!po) {
+        resolve(d);
+      }
+    } catch (err) {
+    }
+  });
+};
+
+
+export const initPerf = async () => {
+  getFID().then((time => {
+    sendPerf({
+      fid: time,
+    })
+  }))
+  const fp = await getFP()
+  const fcp = await getFCP()
+  const lcp = await getLCP()
+  const navigationObj = await getNavigation()
   sendPerf({
     fp,
     fcp,
     lcp,
-    dns,
-    tcp,
-    http,
-    dom,
-    load,
-  })
-}
-
-export const initPerf = () => {
-
-  const observer = new PerformanceObserver(callback)
-  observer.observe({
-    entryTypes: ['paint', 'largest-contentful-paint', 'navigation', 'first-input'],
+    ...navigationObj,
   })
 }
 
