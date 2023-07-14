4fcc7cb80c728f696b787b54353808b3035c9b47
supercll <1033581609@qq.com>
2023-07-14 12:08:48 +0800

    build: vite build

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { createHtmlPlugin } from 'vite-plugin-html'
+const NODE_ENV = process.env.NODE_ENV
+const isProduction = NODE_ENV === 'production'
+export default defineConfig({
+  build: {
+    outDir: 'dist-vite',
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'iacg-monitor',
+      // the proper extensions will be added
+      fileName: 'iacg-monitor',
+    },
+    // rollupOptions: {
+    //   output: {
+    //     chunkFileNames: 'js/[name]-[hash].js',
+    //     entryFileNames: 'js/[name]-[hash].js',
+    //     assetFileNames: '[ext]/name-[hash].[ext]',
+    //   },
+    // },
+  },
+  plugins: [
+    !isProduction &&
+      createHtmlPlugin({
+        minify: true,
+        pages: [
+          {
+            filename: 'iacg-monitor',
+            template: 'index.html',
+            entry: 'src/index.js',
+          },
+        ],
+      }),
+  ],
+})
